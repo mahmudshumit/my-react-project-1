e54b4fcf6751a8f9091d9b07c1d57905df934813
mahmudshumit <rahmanshumit@gmail.com>
2022-09-20 06:10:36 +0600

    add

--- src/Pages/LogIn/Social/Social.js
+++ src/Pages/LogIn/Social/Social.js
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap';
 import glogo from '../../../images/glogo.png';
 import flogo from '../../../images/flogo.png';
 import gitlogo from '../../../images/gitlogo.png';
-import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
@@ -24,19 +24,23 @@ const Social = () => {
   ///github authentication
   const [signInWithGithub, gituser, gitloading, giterror] = useSignInWithGithub(auth);
 
-if(loading ||gitloading){
+
+  //facebook
+  const [signInWithFacebook, fuser, floading, ferror] = useSignInWithFacebook(auth);
+
+if(loading ||gitloading ||floading){
     return <Loading></Loading>
 }
   let errorSocial;
-  if (error || giterror) {
+  if (error || giterror ||ferror) {
       
           errorSocial= 
-          <p className='text-danger'>Error: {error?.message}   {giterror?.message}</p>
+          <p className='text-danger'>Error: {error?.message}   {giterror?.message} {ferror?.message}</p>
        
   
       }
 
-if(user || gituser){
+if(user || gituser||fuser){
   navigate('/home');
 }
 
@@ -53,7 +57,7 @@ if(user || gituser){
             <img style={{ width: '20px' }} src={glogo} alt="" />
                     <span className='px-2'>Google Sign In</span>
                     </Button>
-                    <Button  variant="outline-primary w-70 d-block mx-auto my-3">
+                    <Button onClick={() => signInWithFacebook()}  variant="outline-primary w-70 d-block mx-auto my-3">
             <img style={{ width: '20px' }} src={flogo} alt="" />
                     <span className='px-2'>Google Sign In</span>
                     </Button>
